1ff01898c89009474ad9de2b2951a1ef84d9cbad
Reiq.Laptop <reiqvideos@gmail.com>
2021-10-17 02:37:56 -0400

    Maquetación de Index y Tarjetas para los pokemones

--- src/views/index.jsx
+++ src/views/index.jsx
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
+import CardPokemon from "./partials/card-pokemon";
 
 class Index extends Component {
     render() {
         return (
             <React.Fragment>
-                
+                <main>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-6 offset-md-3 mb-5">
+                                <div className="input-group flex-nowrap rounded overflow-hidden shadow">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text bg-danger text-light">Pokémon</span>
+                                    </div>
+                                    <input type="text" className="form-control" placeholder="Name or ID" autoFocus aria-describedby="addon-wrapping" />
+                                    <div className="input-group-prepend">
+                                        <button className="btn btn-dark">Search</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <CardPokemon />
+                    </div>
+                </main>
             </React.Fragment>
         )
     }
--- src/views/partials/header.jsx
+++ src/views/partials/header.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Header () {
     return(
         <header>
-            <div className="container text-center">
-                <h1 className="mb-0 display-2">Pokédex</h1>
+            <div className="container text-center pb-5 pt-3">
+                <h1 className="mb-0 display-3">Pokédex</h1>
                 <p className="mb-0 h2">Online and easy to use</p>
             </div>
         </header>
