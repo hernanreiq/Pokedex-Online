7c6887d530f401a31226a8df568d7641bf007279
Reiq.Laptop <reiqvideos@gmail.com>
2021-10-18 08:45:21 -0400

    Corregido error ortográfico

--- src/views/partials/header.jsx
+++ src/views/partials/header.jsx
@@ -6,7 +6,7 @@ function Header () {
             <div className="container text-center pb-4 pt-3">
                 <h1 className="mb-0 display-4">Pokédex</h1>
                 <p className="mb-0 h2">Online and easy to use</p>
-                <p className="mb-0 firm">Development and Web design by: <span className="badge badge-dark"><a href="https://bit.ly/hernanreiq" target="_blank" className="text-light" rel="noreferrer">Hernan.Reiq</a></span></p>
+                <p className="mb-0 firm">Development and Web Design by: <span className="badge badge-dark"><a href="https://bit.ly/hernanreiq" target="_blank" className="text-light" rel="noreferrer">Hernan.Reiq</a></span></p>
             </div>
             <div className="black-division"></div>
         </header>
