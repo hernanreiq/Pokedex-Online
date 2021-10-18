611a368c31442e7e41dc68a0a6e44f4e433e0147
Reiq.Laptop <reiqvideos@gmail.com>
2021-10-18 04:52:07 -0400

    Tipo de pokémon representado con insignias

--- src/views/helpers/functions.jsx
+++ src/views/helpers/functions.jsx
@@ -16,3 +16,78 @@ export const TransformTextStrings = (text) => {
     }
     return result;
 }
+
+export const PokemonImage = (PokemonType) => {
+    var result = '';
+    switch (PokemonType) {
+        case 'Normal':
+            result = 'https://cdn2.bulbagarden.net/upload/9/95/Normal_icon_SwSh.png';
+            break;
+        case 'Fighting':
+            result = 'https://cdn2.bulbagarden.net/upload/3/3b/Fighting_icon_SwSh.png';
+            break;
+        case 'Flying':
+            result = 'https://cdn2.bulbagarden.net/upload/b/b5/Flying_icon_SwSh.png';
+            break;
+        case 'Poison':
+            result = 'https://cdn2.bulbagarden.net/upload/8/8d/Poison_icon_SwSh.png';
+            break;
+        case 'Ground':
+            result = 'https://cdn2.bulbagarden.net/upload/2/27/Ground_icon_SwSh.png';
+            break;
+        case 'Rock':
+            result = 'https://cdn2.bulbagarden.net/upload/1/11/Rock_icon_SwSh.png';
+            break;
+        case 'Bug':
+            result = 'https://cdn2.bulbagarden.net/upload/9/9c/Bug_icon_SwSh.png';
+            break;
+        case 'Ghost':
+            result = 'https://cdn2.bulbagarden.net/upload/0/01/Ghost_icon_SwSh.png';
+            break;
+        case 'Steel':
+            result = 'https://cdn2.bulbagarden.net/upload/0/09/Steel_icon_SwSh.png';
+            break;
+        case 'Fire':
+            result = 'https://cdn2.bulbagarden.net/upload/a/ab/Fire_icon_SwSh.png';
+            break;
+        case 'Water':
+            result = 'https://cdn2.bulbagarden.net/upload/8/80/Water_icon_SwSh.png';
+            break;
+        case 'Grass':
+            result = 'https://cdn2.bulbagarden.net/upload/a/a8/Grass_icon_SwSh.png';
+            break;
+        case 'Electric':
+            result = 'https://cdn2.bulbagarden.net/upload/7/7b/Electric_icon_SwSh.png';
+            break;
+        case 'Psychic':
+            result = 'https://cdn2.bulbagarden.net/upload/7/73/Psychic_icon_SwSh.png';
+            break;
+        case 'Ice':
+            result = 'https://cdn2.bulbagarden.net/upload/1/15/Ice_icon_SwSh.png';
+            break;
+        case 'Dragon':
+            result = 'https://cdn2.bulbagarden.net/upload/7/70/Dragon_icon_SwSh.png';
+            break;
+        case 'Dark':
+            result = 'https://cdn2.bulbagarden.net/upload/d/d5/Dark_icon_SwSh.png';
+            break;
+        case 'Fairy':
+            result = 'https://cdn2.bulbagarden.net/upload/c/c6/Fairy_icon_SwSh.png';
+            break;
+        default:
+            result = 'https://cdn2.bulbagarden.net/upload/9/95/Normal_icon_SwSh.png';
+    }
+    return result;
+}
+
+export const ColStyles = (PokemonTypesLength) => {
+    var result = '';
+    if (PokemonTypesLength === 1) {
+        result = 'col-md-12';
+    } else if (PokemonTypesLength === 2) {
+        result = 'col-md-6';
+    } else {
+        result = 'col-md-4';
+    }
+    return result;
+}
--- src/views/partials/card-types.jsx
+++ src/views/partials/card-types.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { CapitalizeFirstLetter } from "../helpers/functions";
+import { CapitalizeFirstLetter, PokemonImage, ColStyles } from "../helpers/functions";
 
 class CardTypes extends Component {
     render() {
@@ -10,14 +10,19 @@ class CardTypes extends Component {
                         <h2 className="card-title mb-0 text-center">Type</h2>
                     </div>
                     <div className="card-footer text-center">
-                        <h4 className="mb-0">
+                        <div className="row">
                             {this.props.types.map((pokemon, i) => {
                                 return (
-                                    <span className="badge badge-dark ml-1" key={i}>{CapitalizeFirstLetter(pokemon.type.name)}</span>
+                                    <React.Fragment key={i}>
+                                        <div className={ColStyles(this.props.types.length) + ' mx-auto my-2'}>
+                                            <img src={PokemonImage(CapitalizeFirstLetter(pokemon.type.name))} alt={CapitalizeFirstLetter(pokemon.type.name)} /> <br />
+                                            <h4 className="mb-0"><span className="badge badge-dark">{CapitalizeFirstLetter(pokemon.type.name)}</span></h4>
+                                        </div>
+                                    </React.Fragment>
                                 )
                             })
                             }
-                        </h4>
+                        </div>
                     </div>
                 </div>
             </React.Fragment >
